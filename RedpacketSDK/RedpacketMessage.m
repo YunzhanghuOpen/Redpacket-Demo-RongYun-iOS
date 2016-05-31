53f5a8b511f9fd0e037ce233d6efe9c42edea21e
sprhawk <hongbo@yang.me>
2016-05-31 18:28:32 +0800

    和 Android 重新调整了消息格式

--- RedpacketSDK/RedpacketMessage.m
+++ RedpacketSDK/RedpacketMessage.m
@@ -9,13 +9,8 @@
 #import "RedpacketMessage.h"
 #import <RongIMKit/RongIMKit.h>
 
-// 按照 Android 的消息格式修改了消息结构
-static NSString *const SenderUserNameKey = @"sendUserName";
-static NSString *const SenderUserIdKey = @"sendUserID";
-static NSString *const ReceiverUserNameKey = @"receiveUserName";
-static NSString *const ReceiverUserIdKey = @"receiveUserID";
-static NSString *const RedpacketMessageKey = @"message";
-static NSString *const RedpacketIdKey = @"moneyID";
+static NSString *const RedpacketDictKey = @"redpacket";
+static NSString *const UserDictKey = @"user";
 
 @interface RedpacketMessage ()
 @property (nonatomic, readwrite, copy) RedpacketMessageModel *redpacket;
@@ -43,19 +38,21 @@ static NSString *const RedpacketIdKey = @"moneyID";
     
     // 先保证是一个正确的红包消息
     if(self.redpacket) {
-        // 原来使用的是 RedpacketMessagModel 的方法生成的 Dictionary ，但是后来 Android 那边需求
-        // 按照 Android 的消息机制做了修改。
-        dic[SenderUserIdKey] = self.redpacket.redpacketSender.userId;
-        dic[SenderUserNameKey] = self.redpacket.redpacketSender.userNickname;
+        NSDictionary *modelDic = [self.redpacket redpacketMessageModelToDic];
+        [dic addEntriesFromDictionary:modelDic];
         
-        if (RedpacketMessageTypeRedpacket == self.redpacket.messageType) {
-            dic[RedpacketMessageKey] = self.redpacket.redpacket.redpacketGreeting;
-            dic[RedpacketIdKey] = self.redpacket.redpacketId;
-        }
-        else // RedpacketMessageTypeTedpacketTakenMessage (原来的类型就是这个名字)
-        {
-            dic[ReceiverUserIdKey] = self.redpacket.redpacketReceiver.userId;
-            dic[ReceiverUserNameKey] = self.redpacket.redpacketReceiver.userNickname;
+        if (self.redpacketUserInfo) {
+            NSMutableDictionary *userInfoDic = [[NSMutableDictionary alloc] init];
+            if (self.redpacketUserInfo.name) {
+                [userInfoDic setObject:self.redpacketUserInfo.name forKeyedSubscript:@"name"];
+            }
+            if (self.redpacketUserInfo.portraitUri) {
+                [userInfoDic setObject:self.redpacketUserInfo.portraitUri forKeyedSubscript:@"icon"];
+            }
+            if (self.redpacketUserInfo.userId) {
+                [userInfoDic setObject:self.redpacketUserInfo.userId forKeyedSubscript:@"id"];
+            }
+            dic[UserDictKey] = userInfoDic;
         }
         
         if ([NSJSONSerialization isValidJSONObject:dic]) {
@@ -87,47 +84,18 @@ static NSString *const RedpacketIdKey = @"moneyID";
                                                         options:0
                                                           error:&error];
     if ([dic isKindOfClass:[NSDictionary class]]) {
-        // 原来使用的是 RedpacketMessagModel 的方法生成的 Dictionary ，但是后来 Android 那边需求
-        // 按照 Android 的消息机制做了修改。但是这里依然生成 RedpacketMessageModel，以保证原来的功
-        // 能代码尽量保持不变
-        RedpacketMessageModel *redpacket = [[RedpacketMessageModel alloc] init];
-        
-        NSString *senderUserId = dic[SenderUserIdKey];
-        NSString *senderUserName = dic[SenderUserNameKey];
-        RedpacketUserInfo *sender = [[RedpacketUserInfo alloc] init];
-        sender.userNickname = senderUserName;
-        sender.userId = senderUserId;
-        redpacket.redpacketSender = sender;
-        
-        NSString *message = dic[RedpacketMessageKey];
-        NSString *redpacketId = dic[RedpacketIdKey];
-        
-        if (redpacketId) { // 是红包消息
-            redpacket.redpacketId = redpacketId;
-            redpacket.redpacket.redpacketGreeting = message;
-            redpacket.messageType = RedpacketMessageTypeRedpacket;
+        NSDictionary *redpacketDic = dic;
+        if ([RedpacketMessageModel isRedpacketRelatedMessage:redpacketDic]) {
+            RedpacketMessageModel *redpacket = [RedpacketMessageModel redpacketMessageModelWithDic:redpacketDic];
+            self.redpacket = redpacket;
         }
-        else
-        {
-            NSString *userId = dic[ReceiverUserIdKey];
-            NSString *userName = dic[ReceiverUserNameKey];
-            
-            RedpacketUserInfo *receiver = [[RedpacketUserInfo alloc] init];
-            receiver.userNickname = userName;
-            receiver.userId = userId;
-            redpacket.redpacketReceiver = receiver;
-            redpacket.messageType = RedpacketMessageTypeTedpacketTakenMessage;
+        else {
+            NSLog(@"获取的不是红包相关的数据");
         }
-        
-        self.redpacket = redpacket;
-        self.redpacketUserInfo = [[RCUserInfo alloc] initWithUserId:senderUserId
-                                                               name:senderUserName
-                                                           portrait:nil];
     }
     else {
-        NSLog(@"获取的不是红包相关的数据");
+        NSLog(@"获取的 JSON 不是字典内容");
     }
-    
 }
 
 - (NSString *)conversationDigest
@@ -159,7 +127,7 @@ static NSString *const RedpacketIdKey = @"moneyID";
                   NSLocalizedString(@"的红包", @"领取红包消息结尾")
                   ];
             
-           
+            
         }
     }
     return tip;
